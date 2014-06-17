12c5541bd672b284345bdfc49ac3bbc4df32e868
Evan Chessman <chessman_evan@wheatoncollege.edu>
2014-06-17 13:16:36 -0400

    working on a chart with area based on time data

--- public/js/cinemap/cinemap-showings.js
+++ public/js/cinemap/cinemap-showings.js
@@ -50,4 +50,69 @@ $(document).ready(function(){
 	//This will get the first returned node in the jQuery collection.
 	var myNewChart = new Chart(ctx).Line(data);  
   });
+
+  var chartData = generateChartData();
+
+  var chart = AmCharts.makeChart("chartdiv", {
+    "type": "serial",
+    "theme": "dark",
+    "pathToImages": "http://www.amcharts.com/lib/3/images/",
+    "dataProvider": chartData,
+    "valueAxes": [{
+        "position": "left",
+        "title": "Unique visitors"
+    }],
+    "graphs": [{
+        "fillAlphas": 0.4,
+        "valueField": "visits"
+    }],
+    "chartScrollbar": {},
+    "chartCursor": {
+        "categoryBalloonDateFormat": "JJ:NN, DD MMMM",
+        "cursorPosition": "mouse"
+    },
+    "categoryField": "date",
+    "categoryAxis": {
+        "minPeriod": "mm",
+        "parseDates": true
+    }
+  });
+
+  chart.addListener("dataUpdated", zoomChart);
+  // when we apply theme, the dataUpdated event is fired even before we add listener, so
+  // we need to call zoomChart here
+  zoomChart();
+  // this method is called when chart is first inited as we listen for "dataUpdated" event
+  function zoomChart() {
+      // different zoom methods can be used - zoomToIndexes, zoomToDates, zoomToCategoryValues
+      chart.zoomToIndexes(chartData.length - 250, chartData.length - 1);
+  }
+
+  // generate some random data, quite different range
+  function generateChartData() {
+    var chartData = [];
+    // current date
+    var firstDate = new Date();
+    // now set 500 minutes back
+    firstDate.setMinutes(firstDate.getDate() - 1000);
+
+    // and generate 500 data items
+    for (var i = 0; i < 500; i++) {
+      var newDate = new Date(firstDate);
+      // each time we add one minute
+      newDate.setMinutes(newDate.getMinutes() + i);
+      // some random number
+      var visits = Math.round(Math.random() * 40 + 10 + i + Math.random() * i / 5);
+      // add data item to the array
+      chartData.push({
+          date: newDate,
+           visits: visits
+      });
+    }
+    return chartData;
+  }
 });
+
+
+
+
