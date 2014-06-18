16c295d3f3420279f1d9f5492d0ed35633529918
Evan Chessman <chessman_evan@wheatoncollege.edu>
2014-06-18 12:08:19 -0400

    took out chat that tom created and the table displaying all the showtimes with the id of the venues and features

--- public/js/cinemap/cinemap-showings.js
+++ public/js/cinemap/cinemap-showings.js
@@ -1,15 +1,3 @@
-$(document).ready(function(){
-  $.ajax({
-     type: 'GET',
-     url: 'http://www.cinemap.io/api/0.1/showings',
-     success: function(data) {
-       for (var item in data) {
-         $("#showings-listing").append("<tr><td>" + data[item].venue_id  + "</td><td>" + data[item].feature_id + "</td><td>" + (new Date(data[item].showtime)).toUTCString() + "</td></tr>");
-       }
-     }
-   });
-});
-
 // jQuery UI Datepicker JS init
 var datepickerSelector = '#datepicker-01';
 $(datepickerSelector).datepicker({
@@ -27,29 +15,33 @@ $(datepickerSelector).datepicker('widget').css({'margin-left': -$(datepickerSele
 
 
 $(document).ready(function(){
-  var data = {
-    labels: [],
-    datasets: [{
-    fillColor : "rgba(151,187,205,0.5)",
-    strokeColor : "rgba(151,187,205,1)",
-    pointColor : "rgba(151,187,205,1)",
-    pointStrokeColor : "#eee",
-    data : []
-      }]
-    };
-      $.getJSON("http://www.cinemap.io/api/0.1/counts/showtimes", function(json) {
-          for (var i = 0; i < json.length; i++) {
-            data.labels.push(json[i]["_id"]);
-            data.datasets[0].data.push(json[i]["count"]);
-          }
 
-    //Get context with jQuery - using jQuery's .get() method.
-	var ctx = $("#showtimes-line").get(0).getContext("2d");
-	$("#showtimes-line").get(0).setAttribute('width', '1200');
-	$("#showtimes-line").get(0).setAttribute('height', '800');
-	//This will get the first returned node in the jQuery collection.
-	var myNewChart = new Chart(ctx).Line(data);  
-  });
+// Tom's graph
+// must add this line to the hbs to work <div><canvas id="showtimes-line" data-type="Line"></canvas></div>
+
+ //  var data = {
+ //    labels: [],
+ //    datasets: [{
+ //    fillColor : "rgba(151,187,205,0.5)",
+ //    strokeColor : "rgba(151,187,205,1)",
+ //    pointColor : "rgba(151,187,205,1)",
+ //    pointStrokeColor : "#eee",
+ //    data : []
+ //      }]
+ //    };
+ //      $.getJSON("http://www.cinemap.io/api/0.1/counts/showtimes", function(json) {
+ //          for (var i = 0; i < json.length; i++) {
+ //            data.labels.push(json[i]["_id"]);
+ //            data.datasets[0].data.push(json[i]["count"]);
+ //          }
+
+ //    //Get context with jQuery - using jQuery's .get() method.
+	// var ctx = $("#showtimes-line").get(0).getContext("2d");
+	// $("#showtimes-line").get(0).setAttribute('width', '1200');
+	// $("#showtimes-line").get(0).setAttribute('height', '800');
+	// //This will get the first returned node in the jQuery collection.
+	// var myNewChart = new Chart(ctx).Line(data);  
+ //  });
 
 //=========================================================================================================================================================
 
@@ -73,7 +65,7 @@ $(document).ready(function(){
           } // ends for loop
           var chart = AmCharts.makeChart("chartdiv", {
             "type": "serial",
-            "theme": "dark",
+            "theme": "light",
             "pathToImages": "http://www.amcharts.com/lib/3/images/",
             "dataProvider": chartData,
             "valueAxes": [{
