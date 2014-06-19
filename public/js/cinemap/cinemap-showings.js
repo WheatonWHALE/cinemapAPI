739efabbaaaea826f4177cba118f3bcc21ca8b27
Evan Chessman <chessman_evan@wheatoncollege.edu>
2014-06-19 11:46:16 -0400

    working on chart that compares movie ratings and also uses a datepicker

--- public/js/cinemap/cinemap-showings.js
+++ public/js/cinemap/cinemap-showings.js
@@ -8,7 +8,9 @@ $(datepickerSelector).datepicker({
 }).prev('.btn').on('click', function (e) {
   e && e.preventDefault();
   $(datepickerSelector).focus();
+
 });
+
 
 // Now let's align datepicker with the prepend button
 $(datepickerSelector).datepicker('widget').css({'margin-left': -$(datepickerSelector).prev('.btn').outerWidth()});
@@ -49,18 +51,56 @@ $(document).ready(function(){
 // am chart \
 // ----------
 
+  var G = [];
+  var PG = [];
+  var PG13 = [];
+  var R = [];
   var chartData = [];
+
   $.getJSON("http://www.cinemap.io/api/0.1/counts/showtimes", function(json) {
           for (var i = 0; i < json.length; i++) {
             var newDate = json[i]["_id"];
             // set count
             var counts = json[i]["count"];
+
+            var a1 = json[i]["count"];
+            var b1 = json[i]["count"];
+
+            var a2 = json[i]["count"];
+            var b2 = json[i]["count"];
+
+            var a3 = json[i]["count"];
+            var b3 = json[i]["count"];
+
+            var a4 = json[i]["count"];
+            var b4 = json[i]["count"];
+            
             // push data to the array
             console.log("before push");
             chartData.push({
               date: newDate,
               counts: counts
             }); // ends chartData.push
+            G.push({
+              date: newDate,
+              value: a1,
+              volume: b1
+            }); // ends pushing to G
+            PG.push({
+              date: newDate,
+              value: a2,
+              volume: b2
+            }); // ends PG.push
+            PG13.push({
+              date: newDate,
+              value: a3,
+              volume: b3
+            }); // ends PG13
+            R.push({
+              date: newDate,
+              value: a4,
+              volume: b4
+            }); // ends pushing to R
 
           } // ends for loop
           var chart = AmCharts.makeChart("chartdiv", {
@@ -88,6 +128,144 @@ $(document).ready(function(){
             }
           });
 
+          var chart2 = AmCharts.makeChart("chartdiv2", {
+            type: "stock",
+              "theme": "none",
+              pathToImages: "http://www.amcharts.com/lib/3/images/",
+
+            dataSets: [{
+                title: "Rated G Movies",
+                fieldMappings: [{
+                  fromField: "value",
+                  toField: "value"
+                }, {
+                  fromField: "volume",
+                  toField: "volume"
+                }],
+                dataProvider: G,
+                categoryField: "date"
+              },
+
+              {
+                title: "second data set",
+                fieldMappings: [{
+                  fromField: "value",
+                  toField: "value"
+                }, {
+                  fromField: "volume",
+                  toField: "volume"
+                }],
+                dataProvider: PG,
+                categoryField: "date"
+              },
+
+              {
+                title: "third data set",
+                fieldMappings: [{
+                  fromField: "value",
+                  toField: "value"
+                }, {
+                  fromField: "volume",
+                  toField: "volume"
+                }],
+                dataProvider: PG13,
+                categoryField: "date"
+              },
+
+              {
+                title: "fourth data set",
+                fieldMappings: [{
+                  fromField: "value",
+                  toField: "value"
+                }, {
+                  fromField: "volume",
+                  toField: "volume"
+                }],
+                dataProvider: R,
+                categoryField: "date"
+              }
+            ],
+
+            panels: [{
+
+                showCategoryAxis: false,
+                title: "Value",
+                percentHeight: 70,
+
+                stockGraphs: [{
+                  id: "g1",
+
+                  valueField: "value",
+                  comparable: true,
+                  compareField: "value",
+                  balloonText: "[[title]]:<b>[[value]]</b>",
+                  compareGraphBalloonText: "[[title]]:<b>[[value]]</b>"
+                }],
+
+                stockLegend: {
+                  periodValueTextComparing: "[[percents.value.close]]%",
+                  periodValueTextRegular: "[[value.close]]"
+                }
+              },
+
+              {
+                title: "Volume",
+                percentHeight: 30,
+                stockGraphs: [{
+                  valueField: "volume",
+                  type: "column",
+                  showBalloon: false,
+                  fillAlphas: 1
+                }],
+
+
+                stockLegend: {
+                  periodValueTextRegular: "[[value.close]]"
+                }
+              }
+            ],
+
+            chartScrollbarSettings: {
+              graph: "g1"
+            },
+
+            chartCursorSettings: {
+              valueBalloonsEnabled: true,
+                  fullWidth:true,
+                  cursorAlpha:0.1
+            },
+
+            periodSelector: {
+              position: "left",
+              periods: [{
+                period: "MM",
+                selected: true,
+                count: 1,
+                label: "1 month"
+              }, {
+                period: "YYYY",
+                count: 1,
+                label: "1 year"
+              }, {
+                period: "YTD",
+                label: "YTD"
+              }, {
+                period: "MAX",
+                label: "MAX"
+              }]
+            },
+
+            dataSetSelector: {
+              position: "left"
+            }
+        });
+
+          chart2.addListener('rendered', function (event) {
+            $( ".amChartsPeriodSelector .amChartsInputField" ).datepicker({
+                dateFormat: "dd-mm-yy"
+            });
+          });
+
           chart.addListener("dataUpdated", zoomChart);
           zoomChart();
           function zoomChart() {
@@ -95,3 +273,7 @@ $(document).ready(function(){
           }
     }); // ends getjson  
 });
+
+// ============================================================================================================================================================
+
+
